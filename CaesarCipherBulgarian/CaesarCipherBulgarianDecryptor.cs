f609c663ccd5739bc007d19b214270dc3693f8f8
Veselin Denchev <den4ev01@gmail.com>
2023-05-21 18:06:18 +0300

    Add project files.

---
+++ CaesarCipherBulgarian/CaesarCipherBulgarianDecryptor.cs
@@ -0,0 +1,50 @@
+﻿namespace CaesarCipherBulgarian
+{
+    using System.Text;
+
+    public static class CaesarCipherBulgarianDecryptor
+    {
+        private const string BULGARIAN_ALPHABET_STRING = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЮЯ";
+
+        private static readonly List<char> bulgarianAlphabetCharArray = BULGARIAN_ALPHABET_STRING.ToCharArray().ToList();
+
+        public static void PrintAllCaesarCipherPossibilities(string cipheredText)
+        {
+            if (!String.IsNullOrWhiteSpace(cipheredText))
+            {
+                StringBuilder decipheredTextPossibilitiesStringBuilder = new StringBuilder();
+                decipheredTextPossibilitiesStringBuilder.AppendLine("Възможни варианти за оригиналния текст:");
+
+                for (int i = 1; i < bulgarianAlphabetCharArray.Count; i++)
+                {
+                    foreach (char character in cipheredText)
+                    {
+                        if (bulgarianAlphabetCharArray.Contains(character))
+                        {
+                            int currentCharIndex = bulgarianAlphabetCharArray.IndexOf(character);
+                            decipheredTextPossibilitiesStringBuilder.Append(bulgarianAlphabetCharArray[(currentCharIndex + i) % bulgarianAlphabetCharArray.Count]);
+                        }
+                        else if (character == ' ')
+                        {
+                            decipheredTextPossibilitiesStringBuilder.Append(character);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Текстът съдържа символ, който не е от българската азбука!");
+                            break;
+                        }
+                    }
+
+                    decipheredTextPossibilitiesStringBuilder.AppendLine();
+                }
+
+                string decipheredTextPossibilities = decipheredTextPossibilitiesStringBuilder.ToString();
+                Console.WriteLine(decipheredTextPossibilities);
+            }
+            else
+            {
+                Console.WriteLine("Празен текст!");
+            }
+        }
+    }
+}
---
+++ CaesarCipherBulgarian/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+using CaesarCipherBulgarian;
+
+Console.InputEncoding = UTF8Encoding.Unicode;
+Console.OutputEncoding = UTF8Encoding.Unicode;
+
+Console.WriteLine("Въведeтe текст с български букви, шифриран чрез шифър на Цезар, който да бъде дешифриран: ");
+string input = Console.ReadLine().ToUpper();
+
+Console.WriteLine();
+
+CaesarCipherBulgarianDecryptor.PrintAllCaesarCipherPossibilities(input);
